ec2c81da27c6a8c76bc5fc8c629fe427a4948db8
thegeets <thegeets86@gmail.com>
2024-12-31 08:23:35 +0545

    add new file

---
+++ NOT GATE/not_gate.vhdl
@@ -0,0 +1,13 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity not_gate is
+    port ( a : in std_logic;
+           y : out std_logic);
+end not_gate;
+
+architecture behavioral of not_gate is
+begin
+    y <= not a;
+end behavioral;
